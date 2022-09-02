dbdfd1634e281f64ff072eac7bc73172f0233447
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2022-09-02 15:17:47 +0100

    SVG Completed

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -1,34 +1,85 @@
-
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
-contract DynamicSvgNft is ERC721{
+import "hardhat/console.sol";
+
+error ERC721Metadata__URI_QueryFor_NonExistentToken();
+
+contract DynamicSvgNft is ERC721, Ownable {
     uint256 private s_tokenCounter;
-    string private immutable i_lowSvg;
-    string private immutable i_highSvg;
-    string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
-    constructor(string memory lowSvg, string memory highSvg)ERC721("DynamicSvgNft","DSN"){
+    string private s_lowImageURI;
+    string private s_highImageURI;
+
+    mapping(uint256 => int256) private s_tokenIdToHighValues;
+    AggregatorV3Interface internal immutable i_priceFeed;
+    event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
-        i_highSvg =highSvg;
-        i_lowSvg = lowSvg;
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+        // setLowSVG(lowSvg);
+        // setHighSVG(highSvg);
+        s_lowImageURI = svgToImageURI(lowSvg);
+        s_highImageURI = svgToImageURI(highSvg);
     }
-    function mintNft() public {
+
+    // function setLowURI(string memory svgLowURI) public onlyOwner {
+    //     s_lowImageURI = svgLowURI;
+    // }
+
+    // function setHighURI(string memory svgHighURI) public onlyOwner {
+    //     s_highImageURI = svgHighURI;
+    // }
+
+    // function setLowSVG(string memory svgLowRaw) public onlyOwner {
+    //     string memory svgURI = svgToImageURI(svgLowRaw);
+    //     setLowURI(svgURI);
+    // }
+
+    // function setHighSVG(string memory svgHighRaw) public onlyOwner {
+    //     string memory svgURI = svgToImageURI(svgHighRaw);
+    //     setHighURI(svgURI);
+    // }
+
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValues[s_tokenCounter] = highValue;
         _safeMint(msg.sender, s_tokenCounter);
-        s_tokenCounter+= s_tokenCounter;
-    } 
-      function _baseURI() internal pure override returns (string memory) {
+        s_tokenCounter = s_tokenCounter + 1;
+        emit CreatedNFT(s_tokenCounter, highValue);
+    }
+
+    // You could also just upload the raw SVG and have solildity convert it!
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        // example:
+        // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
+        // would return ""
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
         return "data:application/json;base64,";
     }
 
-    function svgToImageUrl(string memory svg) public pure returns (string memory){
-        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-        return string(abi.encodePacked((base64EncodedSvgPrefix, svgBase64Encoded)));
-    }
-function tokenURI (uint256 tokenId) public view override returns (string memory){
-    require(_exists(tokenId), "URI Query for nonexistent token");
-      return
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) {
+            revert ERC721Metadata__URI_QueryFor_NonExistentToken();
+        }
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = s_lowImageURI;
+        if (price >= s_tokenIdToHighValues[tokenId]) {
+            imageURI = s_highImageURI;
+        }
+        return
             string(
                 abi.encodePacked(
                     _baseURI(),
@@ -46,5 +97,21 @@ function tokenURI (uint256 tokenId) public view override returns (string memory)
                     )
                 )
             );
-}
+    }
+
+    function getLowSVG() public view returns (string memory) {
+        return s_lowImageURI;
+    }
+
+    function getHighSVG() public view returns (string memory) {
+        return s_highImageURI;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return i_priceFeed;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
